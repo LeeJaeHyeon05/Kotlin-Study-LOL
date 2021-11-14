32ad71f5a28eff436d88cf6b67a689f5787b6cd4
Chris <asonetu11@gmail.com>
2021-11-14 15:58:01 +0900

    change to kotlin code style

--- app/src/main/java/com/example/firstapp/fragment/ItemFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/ItemFragment.kt
@@ -6,20 +6,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.firstapp.R
 import com.example.firstapp.adapter.ItemListAdapter
+import com.example.firstapp.databinding.FragmentItemBinding
 import com.example.firstapp.model.Data
 import com.example.firstapp.model.ItemJson
 import com.example.firstapp.util.getJsonDataFromAsset
 import com.google.gson.Gson
 
 class ItemFragment : Fragment() {
+
+    private lateinit var binding: FragmentItemBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_item, container, false)
+        binding = FragmentItemBinding.inflate(layoutInflater)
 
         val jsonFileString = getJsonDataFromAsset(requireActivity().applicationContext, "item.json")
         val gson = Gson()
@@ -27,13 +29,13 @@ class ItemFragment : Fragment() {
         val data: Map<String, Data> = itemJson.data
         for((key, value) in data) value.id = key
         val dataList = data.map { it.value }
-        val mItemListAdapter = ItemListAdapter(dataList)
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.itemList)
-        recyclerView.adapter = mItemListAdapter
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 5)
+        binding.itemList.run {
+            adapter = ItemListAdapter(dataList)
+            layoutManager = GridLayoutManager(requireContext(), 5)
+        }
 
-        return view
+        return binding.root
     }
 
 }
