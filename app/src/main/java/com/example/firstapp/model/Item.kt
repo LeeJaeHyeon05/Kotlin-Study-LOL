2df0251b56591d2024e31029e6cac8551e8f2f63
Chris <asonetu11@gmail.com>
2021-11-21 19:21:04 +0900

    fix null check error

--- app/src/main/java/com/example/firstapp/model/Item.kt
+++ app/src/main/java/com/example/firstapp/model/Item.kt
@@ -11,21 +11,21 @@ data class ItemJson(
     val data: Map<String, Data>,
 )
 
-@Entity
+@Entity(tableName = "item")
 data class Data(
     @PrimaryKey var id: String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "colloq") val colloq: String,
     @ColumnInfo(name = "plaintext") val plaintext: String,
-    @ColumnInfo(name = "into") val into: List<String>,
+    @ColumnInfo(name = "into") val into: List<String>?,
     @Ignore val image: Image?,
     @Ignore val gold: Gold?,
     @ColumnInfo(name = "tags") val tags: List<String>,
     @Ignore val maps: Map<String, Boolean>?,
     @Ignore val stats: Map<String, String>?,
 ) {
-    constructor(id: String, name: String, description: String, colloq: String, plaintext: String, into: List<String>, tags: List<String>) :
+    constructor(id: String, name: String, description: String, colloq: String, plaintext: String, into: List<String>?, tags: List<String>) :
             this(id, name, description, colloq, plaintext, into, null, null, tags, null, null)
 }
 
