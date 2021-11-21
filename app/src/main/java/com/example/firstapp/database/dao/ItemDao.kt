922978fd7b85decf7232fd6b916b3b0d5c991eeb
Chris <asonetu11@gmail.com>
2021-11-21 19:15:29 +0900

    add select query on ItemDao.kt

--- app/src/main/java/com/example/firstapp/database/dao/ItemDao.kt
+++ app/src/main/java/com/example/firstapp/database/dao/ItemDao.kt
@@ -2,10 +2,17 @@ package com.example.firstapp.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.firstapp.model.Data
 
 @Dao
 interface ItemDao {
+    @Query("SELECT * FROM item")
+    fun selectAll(): List<Data>
+
+    @Query("SELECT count(*) FROM item")
+    fun selectAllCount(): Int
+
     @Insert
     fun insertAll(vararg items: Data)
 }
