de9ce48a720faa8c6561ebf09c392363c4c32bb3
dongk00 <dddongk00@gmail.com>
2021-11-24 21:38:38 +0900

    Retrofit2 추가

--- app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
@@ -1,12 +1,22 @@
 package com.example.firstapp.fragment.build
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.firstapp.databinding.FragmentBuildMainBinding
+import com.example.firstapp.fragment.build.retrofit.RetrofitData
+import com.example.firstapp.fragment.build.retrofit.championData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
 
 class BuildMainFragment : Fragment() {
@@ -25,9 +35,35 @@ class BuildMainFragment : Fragment() {
         binding.buildMainRv.layoutManager = GridLayoutManager(
             this.context,4, GridLayoutManager.VERTICAL, false)
 
+        getAPI()
         return binding.root
     }
 
+    private fun getAPI() {
+
+        GlobalScope.launch(Dispatchers.IO){
+            try{
+                val call = RetrofitData.networkService.getChampionList()
+                call?.enqueue(object : Callback<championData> {
+                    override fun onResponse(
+                        call: Call<championData>,
+                        response: Response<championData>
+                    ) {
+                        var championList = response?.body()?.data
+
+                        Log.d("test", "Success : $championList")
+                    }
+
+                    override fun onFailure(call: Call<championData>, t: Throwable) {
+                        Log.d("test", "Failure : $t")
+                    }
+
+                })
+            } catch (e: Exception){
+                Log.d("test", "Error: ..... $e")
+            }
+        }
+    }
     private fun dummy(): MutableList<String> {
         val dummyList = mutableListOf<String>(
             "가렌", "갈리오", "갱플랭크", "그라가스",
--- app/src/main/java/com/example/firstapp/fragment/build/retrofit/NetworkService.kt
+++ app/src/main/java/com/example/firstapp/fragment/build/retrofit/NetworkService.kt
@@ -1,9 +1,34 @@
 package com.example.firstapp.fragment.build.retrofit
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Url
+import java.util.*
 
 interface NetworkService {
-    //http://ddragon.leagueoflegends.com/cdn/11.23.1/data/ko_KR/champion/Aatrox.json
+    //http://ddragon.leagueoflegends.com/cdn/11.23.1/data/ko_KR/champion.json
+    @GET("/cdn/11.23.1/data/ko_KR/champion.json")
+    fun getChampionList() : Call<championData>
 }
+
+data class championData(
+    val type: String,
+    val foramt : String,
+    val version: String,
+    val data : Objects
+)
+
+data class championData_indiv(
+    val version: String,
+    val id: String,
+    val key: Int,
+    val name: String,
+    val title: String,
+    val blurb: String,
+    val info: Object,
+    val image: Object,
+    val tags: Object,
+    val partype: String,
+    val stats : Object
+)
