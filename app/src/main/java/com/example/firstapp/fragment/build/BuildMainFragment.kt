0f6c2f1d0d1aa77a4e5d4ab9bc8f87578602406d
fresco <63629853+choi-woo-sung@users.noreply.github.com>
2021-12-01 18:03:18 +0900

    룬페이지 제작중
    레이아웃 생성

--- app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
@@ -20,21 +20,7 @@ class BuildMainFragment : BaseFragment<FragmentBuildMainBinding>(R.layout.fragme
 
     private val viewModel: BuildViewModel by viewModels()
 
-    private fun dummy(): MutableList<String> {
-        val dummyList = mutableListOf<String>(
-            "가렌", "갈리오", "갱플랭크", "그라가스",
-            "그레이브즈", "그웬", "나르", "나미",
-            "나서스", "노틸러스", "녹턴", "누누와 윌럼프",
-            "니달리", "니코", "다리우스", "다이애나",
-            "드레이븐", "라이즈", "라칸", "람머스",
-            "럭스", "럼블", "레넥톤", "레오나",
-            "렉사이", "렐", "렝가", "루시안",
-            "룰루", "르블랑", "리신", "리븐",
-            "리산드라", "릴리아", "마스터 이", "마오카이",
-            "말자하", "말파이트", "모데카이저", "모르가나"
-        )
-        return dummyList
-    }
+
 
     override fun init() {
         repeatOnStarted {
