e1c48f3a2e511a736a3cb49a27fc8a6294b272e8
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-12-14 23:46:16 +0900

    SkinFragment

--- app/src/main/java/com/example/firstapp/fragment/skin/SkinFragmemt.kt
+++ app/src/main/java/com/example/firstapp/fragment/skin/SkinFragmemt.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp.fragment.skin
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +11,18 @@ import com.example.firstapp.R
 import com.example.firstapp.adapter.skin.SkinPagerFragmentAdapter
 import com.example.firstapp.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class SkinFragmemt : Fragment() {
+
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
 
 
 
@@ -57,6 +62,43 @@ class SkinFragmemt : Fragment() {
 //        })
 //
 //        //tablayout attach
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view:View = inflater.inflate(R.layout.fragment_skin, container, false)
+        viewPager = view.findViewById(R.id.skin_pager)
+        tabLayout = view.findViewById(R.id.skin_tab_layout)
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val pagerAdapter = SkinPagerFragmentAdapter(requireActivity())
+
+        pagerAdapter.addFragment(SkinByChampionFragment())
+        pagerAdapter.addFragment(SkinByThemeFragment())
+
+        viewPager?.adapter = pagerAdapter
+        viewPager?.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            //이전에 추가된 콜백 제거
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                Log.e("SkinViewPagerFragment", "Page ${position+1}")
+            }
+        })
+
+        val tabLayoutTextArray = arrayOf("챔피언별", "스킨 테마별")
+        TabLayoutMediator(tabLayout,viewPager){ tab, position ->
+            tab.text = tabLayoutTextArray[position]
+        }.attach()
+
+
 
     }
 
