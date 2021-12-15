018631c8bbe6ea33af4f855477420418c187c38c
fresco <63629853+choi-woo-sung@users.noreply.github.com>
2021-12-15 18:17:38 +0900

    buildfragment  검색설정중
    buildfragment  검색설정중

--- app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/build/BuildMainFragment.kt
@@ -1,9 +1,8 @@
 package com.example.firstapp.fragment.build
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -20,9 +19,34 @@ class BuildMainFragment : BaseFragment<FragmentBuildMainBinding>(R.layout.fragme
 
     private val viewModel: BuildViewModel by viewModels()
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.item_menu, menu)
+        menuItem = menu.getItem(2)
 
+        val menuItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchView: SearchView = menuItem.actionView as SearchView
+        inflater.
+
+         menuItem = menu.getItem(number);
+        menuItem.setIcon(some_icon);
+        menuItem.setTitle(some_text);
+        searchView.queryHint = getString(R.string.champion_name)
+
+        searchView.setOnQueryTextFocusChangeListener { v, hasFocus ->
+            menu.findItem(R.id.action_sort).isVisible = !hasFocus
+        }
+
+
+
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
     override fun init() {
+        setHasOptionsMenu(true)
+
+
         repeatOnStarted {
             viewModel.getChampion().collect { Champion ->
                 val championList = Champion.data.values.toList().sortedBy { it.name }
