328626e02f2dcb96237fb67837470241e5f805d0
SEOYOUNG-cloud <67596451+SEOYOUNG-cloud@users.noreply.github.com>
2021-12-18 18:00:02 +0900

    skinPagerFragmentAdapter

--- app/src/main/java/com/example/firstapp/adapter/skin/SkinPagerFragmentAdapter.kt
+++ app/src/main/java/com/example/firstapp/adapter/skin/SkinPagerFragmentAdapter.kt
@@ -1,12 +1,12 @@
 package com.example.firstapp.adapter.skin
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.firstapp.fragment.skin.SkinByChampionFragment
 import com.example.firstapp.fragment.skin.SkinByThemeFragment
+import com.example.firstapp.fragment.skin.SkinFragment
 
-class SkinPagerFragmentAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity){
+class SkinPagerFragmentAdapter(fragmentActivity: SkinFragment) : FragmentStateAdapter(fragmentActivity){
     var fragments : ArrayList<Fragment> = ArrayList()
 
     override fun getItemCount(): Int {
@@ -16,8 +16,9 @@ class SkinPagerFragmentAdapter(fragmentActivity: FragmentActivity) : FragmentSta
     override fun createFragment(position: Int): Fragment { // 페이지 연결
         return when(position){
             0 -> SkinByChampionFragment()
-            else -> { return SkinByThemeFragment() }
+            else -> SkinByThemeFragment()
         }
+        //return fragments[position]
     }
 
 
@@ -31,6 +32,11 @@ class SkinPagerFragmentAdapter(fragmentActivity: FragmentActivity) : FragmentSta
     fun addFragment(fragment: Fragment){
         fragments.add(fragment)
         notifyItemInserted(fragments.size - 1)
+    }
+
+    fun removeFragment(){
+        fragments.removeLast()
+        notifyItemRemoved(fragments.size)
     }
 
 }
