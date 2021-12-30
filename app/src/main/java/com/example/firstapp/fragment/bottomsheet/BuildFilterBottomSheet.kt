24e9b7a81930eb4d89676d582ab32d06999428d3
choi-woo-sung <63629853+choi-woo-sung@users.noreply.github.com>
2021-12-30 23:00:29 +0900

    feat : add bottomseatFragment
    텝레이아웃으로 구현된 바텀시트 개발(50%)

--- app/src/main/java/com/example/firstapp/fragment/bottomsheet/BuildFilterBottomSheet.kt
+++ app/src/main/java/com/example/firstapp/fragment/bottomsheet/BuildFilterBottomSheet.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.firstapp.databinding.BuildFiterBottomContentBinding
+import com.example.firstapp.databinding.ItemSortBottomSheetContentBinding
+import com.example.firstapp.fragment.build.BuildViewModel
+import com.example.firstapp.model.ItemViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class BuildFilterBottomSheet : BottomSheetDialogFragment() {
 
-    private lateinit var binding : BuildFiterBottomContentBinding
-
+    private var _binding: BuildFiterBottomContentBinding? = null
+    private val binding get() = _binding!!
+    private val buildViewModel: BuildViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        _binding = BuildFiterBottomContentBinding.inflate(layoutInflater)
+        val fgAdapter = FgAdap
+//        binding.tabLayout2.
+        return binding.root
+    }
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
+
