308488ef46e27680d4543b56bd1f3e20b554e8e3
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-01-01 18:55:35 +0900

    Add SummorFragment

--- app/src/main/java/com/example/firstapp/fragment/SummonerFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/SummonerFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.firstapp.R
+import com.example.firstapp.databinding.FragmentSummonerBinding
+
 class SummonerFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val binding = FragmentSummonerBinding.inflate(inflater, container, false)
+
         return inflater.inflate(R.layout.fragment_summoner, container, false)
     }
 }
