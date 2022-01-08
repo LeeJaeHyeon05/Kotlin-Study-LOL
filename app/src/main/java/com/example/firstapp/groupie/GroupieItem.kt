c36f68ce04892bd2c0851e24d8c81431570971a9
Chris <asonetu11@gmail.com>
2022-01-08 23:50:24 +0900

    Add GroupieItem.kt

---
+++ app/src/main/java/com/example/firstapp/groupie/GroupieItem.kt
@@ -0,0 +1,24 @@
+package com.example.firstapp.groupie
+
+import android.view.View
+import com.example.firstapp.R
+import com.example.firstapp.databinding.ItemBinding
+import com.example.firstapp.model.Item
+import com.example.firstapp.util.getBaseImageUrl
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
+
+class GroupieItem(val item: Item) : BindableItem<ItemBinding>() {
+
+    override fun getLayout() = R.layout.item
+
+    override fun bind(binding: ItemBinding, position: Int) {
+        Picasso.get().load("${getBaseImageUrl()}/item/${item.id}.png").into(binding.itemImage)
+        binding.itemName.text = item.name
+    }
+
+    override fun initializeViewBinding(view: View): ItemBinding = ItemBinding.bind(view)
+
+    override fun getSpanSize(spanCount: Int, position: Int): Int = 1
+
+}
