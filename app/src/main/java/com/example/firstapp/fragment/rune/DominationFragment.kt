18d184a8adb746edc42fc21c17540ad74c751395
JaeHyeon <jaehyeon0825@gmail.com>
2022-01-18 17:04:23 +0700

    recyclerview before fix error

--- app/src/main/java/com/example/firstapp/fragment/rune/DominationFragment.kt
+++ app/src/main/java/com/example/firstapp/fragment/rune/DominationFragment.kt
@@ -5,15 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.firstapp.R
+import com.example.firstapp.adapter.summorSpell.CustomAdapter
+import com.example.firstapp.databinding.FragmentRuneDominationBinding
 
 class DominationFragment : Fragment() {
+
+    lateinit var multiAdapter:RuneAdapter
+    val datas = mutableListOf<Item>()
+
+    private var mBinding: FragmentRuneDominationBinding? = null
+    private val binding get() = mBinding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rune_domination, container, false)
+        mBinding = FragmentRuneDominationBinding.inflate(inflater,container,false)
+
+        initRecycler()
+
+        return binding.root
+    }
+
+
+    private fun initRecycler() {
+        multiAdapter = RuneAdapter(requireContext())
+        binding.dominationRecyclerView.adapter = multiAdapter
+
+
+        datas.apply {
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+            add(Item(R.drawable.whimsy, BIG_POSITION))
+
+
+
+        }
+
+
+        multiAdapter.datas = datas
+        multiAdapter.notifyDataSetChanged()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
--- app/src/main/java/com/example/firstapp/fragment/rune/RuneNameItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.firstapp.fragment.rune
-
-data class RuneNameItem(val NameText: Int, val NameDes: Int)
