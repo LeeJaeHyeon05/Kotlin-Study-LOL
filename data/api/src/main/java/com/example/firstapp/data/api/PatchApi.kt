25eee865012068026411a595a2c06bd9168ca897
ljws93 <ljws93@naver.com>
2022-02-08 23:17:23 +0900

    make Patch file for Retrofit

---
+++ data/api/src/main/java/com/example/firstapp/data/api/PatchApi.kt
@@ -0,0 +1,12 @@
+package com.example.firstapp.data.api
+
+import com.example.firstapp.model.summoner.SummonerDTO
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PatchApi {
+//    @GET("")
+//    suspend fun getSummonerDTOBySummonerName(@Path("summonerName") summonerName: String): Response<SummonerDTO>
+
+}
