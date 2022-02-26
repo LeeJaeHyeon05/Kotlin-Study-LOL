e5a7a6afbf4efd7f3a48ec84c2c6d8ab00e53621
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-02-27 00:24:40 +0900

    Add SummonerViewModel.kt

--- app/src/main/java/com/example/firstapp/model/SummonerViewModel.kt
+++ app/src/main/java/com/example/firstapp/model/SummonerViewModel.kt
@@ -1,12 +1,12 @@
 package com.example.firstapp.model
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.firstapp.data.repository.SummonerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SummonerViewModel @Inject constructor(private val summonerRepository: SummonerRepository) :ViewModel(){
+class SummonerViewModel @Inject constructor(private val summonerRepository: SummonerRepository) :
+    ViewModel(){
 
 }
